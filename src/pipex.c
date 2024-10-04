babaff07acaeecb5ec950a99df4e3729d894b237
mfleury <mfleury@student.42barcelona.com>
2024-10-04 15:15:43 +0200

    starting pipex

--- src/pipex.c
+++ src/pipex.c
@@ -6,13 +6,20 @@
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 12:55:43 by mfleury           #+#    #+#             */
-/*   Updated: 2024/10/04 12:56:56 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/10/04 15:15:28 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
+void	exit_error()
+{
+
+}
+
 int	main (int argc, char *argv[])
 {
+	if (argc != 5)
+		exit_error();
 	return (0);
 }
