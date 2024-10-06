b434058583be98bea7f77028e0877f58a81d4ec9
Marc Fleury <mfleury03@gmail.com>
2024-10-06 23:38:53 +0200

    pipe and fork functions being tested

--- include/errors.h
+++ include/errors.h
@@ -6,9 +6,13 @@
 /*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:07:40 by mfleury           #+#    #+#             */
-/*   Updated: 2024/10/05 17:27:53 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/10/06 23:31:58 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define ERR_ARG_NUM "Incorrect number of arguments"
 #define ERR_OPEN_FILE "Error while opening file"
+#define ERR_MISSING_FILE "Error file not found"
+#define ERR_FILE_PERM "Missing read/write permissions"
+#define ERR_PIPE_CREA "Error during pipe creation"
+#define ERR_FORK_CREA "Error during fork creation"
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 12:55:43 by mfleury           #+#    #+#             */
-/*   Updated: 2024/10/05 17:27:53 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/10/06 23:36:14 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,36 @@ void	exit_error(char *str)
 	exit(-1);
 }
 
-int	main (int argc, char *argv[])
+void	check_files(char *path1, char *path2)
 {
-	int	fd;
+	int	fd[2];
 
-	if (argc != 5)
+	fd[0] = access(path1, F_OK);
+	fd[1]= access(path1, F_OK);
+	if (fd[0] == -1  || fd[1] == -1)
+		exit_error(ERR_MISSING_FILE);
+	fd[0] = access(path1, R_OK);
+	fd[1]= access(path2, R_OK & W_OK);
+	if (fd[0] == -1  || fd[1] == -1)
+		exit_error(ERR_FILE_PERM);
+}
+
+int	main (int argc, char *argv[], char **env)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (argc != 2) // back to 5
 		exit_error(ERR_ARG_NUM);
+	check_files(argv[1], argv[1]);	// update 2nd arg
+	fd[0] = open(argv[1], O_RDONLY);
+	fd[1] = open(argv[2], O_WRONLY);
+	if (fd[-0] == -1 || fd[1] == -1)
+		exit_error(ERR_OPEN_FILE);
+	if (pipe(fd) == -1)
+		exit_error(ERR_PIPE_CREA);
+	pid = fork();
+	if (pid == -1)
+		exit_error(ERR_FORK_CREA);
 	return (0);
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		exit_error(ERR_OPEN_FILE)
 }
