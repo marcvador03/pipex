d509015193fe5d0beb2c28f06641e98847598f27
Marc Fleury <mfleury03@gmail.com>
2024-10-08 13:08:26 +0200

    WIP

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 12:55:43 by mfleury           #+#    #+#             */
-/*   Updated: 2024/10/07 13:14:26 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/10/07 19:54:09 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,33 +45,33 @@ int	main (int argc, char *argv[]/*, char *envp[]*/)
 		exit_error(ERR_ARG_NUM);
 	check_files(argv[1], argv[1]);	// update 2nd arg
 	fd[0] = open(argv[1], O_RDONLY);
-	fd[1] = open(argv[2], O_WRONLY);
-	//if (fd[-0] == -1 || fd[1] == -1)
-    //		exit_error(ERR_OPEN_FILE);
+	//fd[1] = open(argv[2], O_WRONLY);
 	if (pipe(p_fd) == -1)
 		exit_error(ERR_PIPE_CREA);
 	pid = fork();
 	if (pid == -1)
 		exit_error(ERR_FORK_CREA);
 	dup2(fd[0], p_fd[0]);
-//	dup2(fd[1], p_fd[1]);
 	if (pid == 0)
 	{
 		close(p_fd[1]);
+		args = ft_split(argv[2] ,' ');
+		if (args == NULL)
+			exit_error(ERR_MALLOC);
 		while (read(p_fd[0], &buf, 1) != 0)
-			write(STDIN_FILENO, &buf, 1);
-		close(p_fd[0]);
-		exit(0);
+		{
+			write(STDOUT_FILENO, &buf, 1);
+			execve(args[0], args, NULL);	
+		}
+	close(p_fd[0]);
+	exit(0);
 	}
 	else
 	{
 		close(p_fd[0]);
-		args = ft_split(argv[2] ,' ');
-		if (args == NULL)
-			exit_error(ERR_MALLOC);
-		execve(args[0], args, NULL);	
+		write(p_fd[1], argv[1], ft_strlen(argv[1]));
 		close(p_fd[1]);
-		wait(NULL);
+		wait(0);
 		exit(0);
 	}
 	return (0);
